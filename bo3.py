dac063f5283acbddfe928104ade9eb1573879155
TomV <tom@viner.tv>
2015-06-04 20:45:48 +0100

    bat move

--- bo3.py
+++ bo3.py
@@ -4,19 +4,26 @@ from collections import namedtuple
 W = 800
 H = 600
 RED = 200, 0, 0
+BLUE = 0, 0, 200
 
 ball = Rect((W/2, H/2), (30, 30))
 Direction = namedtuple('Direction', 'x y')
-ball_dir = Direction(1, 1)
+ball_dir = Direction(5, 5)
+
+bat = Rect((W/2, 0.96 * H), (150, 15))
 
 def draw():
     screen.clear()
     screen.draw.filled_rect(ball, RED)
+    screen.draw.filled_rect(bat, RED)
 
 def on_key_down():
     import sys
     sys.exit()
 
+def on_mouse_move(pos):
+    x, y = pos
+    bat.center = (x, bat.center[1])
 
 def move(ball):
     """returns a new ball at a new position
@@ -28,6 +35,7 @@ def move(ball):
 
     if ball.y > H or ball.y <= 0:
         ball_dir = Direction(ball_dir.x, ball_dir.y * -1)
+
 
 
 def update():
